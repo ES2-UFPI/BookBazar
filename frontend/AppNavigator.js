82b817ef8b0b988a465eb551af4ef4361f2d9803
Lucas <ljmlopes@gmail.com>
2024-05-23 08:40:22 -0300

    Criação da tela de filtros da busca #8

--- frontend/AppNavigator.js
+++ frontend/AppNavigator.js
@@ -1,6 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen'
- 
+import FilterScreen from './FilterScreen';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +8,7 @@ const AppNavigator = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Filter" component={FilterScreen} /> 
     </Stack.Navigator>
   );
 };
