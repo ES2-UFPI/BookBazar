4dbbf2a14883158d244e55328a33647bf0ba2bf1
GuiCavalcanti01 <gui.cavalcanti01@outlook.com>
2024-05-27 23:02:13 -0300

    Issue #10 Recuperacao de Dados do Anuncio

--- backend/bookbazar/bookbazar/settings.py
+++ backend/bookbazar/bookbazar/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'appbookbazar',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'bookbazar.urls'
 
